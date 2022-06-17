6e463e171acf198ca471112a77a43874cfb67c42
Maxson de Santana Carvalho <maxson.carvalho@findes.org.br>
2022-06-17 00:56:34 -0300

    Arrays...

--- Arrays/array4.kt
+++ Arrays/array4.kt
@@ -26,4 +26,30 @@ fun main(){
         println("Salarios Ordenados: "+it)
     }
 
+    println("----------------------------------------------------\n")
+    println("----------------------------------------------------\n")
+    for(salario in salarios2){
+        println("Salarios Ordenados: "+salario)
+    }
+    println("Maximo: ${salarios2.maxOrNull()}") //maximo
+    println("Minimo: ${salarios2.minOrNull()}") //minimo 
+    println("Soma: ${salarios2.sum()}") //soma
+    println("Media: ${salarios2.average()}") //media
+
+
+    println("----------------------------------------------------\n")
+
+    val salariosMaioresQue2000 = salarios2.filter { it > 2000 }
+    salariosMaioresQue2000.forEach{
+        println("Salarios Maiores que 2000: "+it)
+    }
+
+    println("----------------------------------------------------\n")
+
+    println("Salarios entre 2 e 5: "+salarios2.count{it in 2000.0..5000.0}) //conta quantos salarios estao entre 2000 e 5000
+    println("Salario encontrado: "+salarios2.find{it == 2000.0}) //procura o primeiro salario que eh 2000 e retorna o valor
+    println("Encontrar Qualquer Salario: "+salarios2.any{it == 2000.0}) //verifica se existe algum salario que eh 2000 e retorna true ou false
+
+
+
 }
